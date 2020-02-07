d9f90b34ef7a3d5233a57e7326d59b14c856b834
Gabriel Vasconcellos <gavapim@gmail.com>
2020-02-07 19:04:25 -0300

    Improving PrintBoard. Changing the black pieces view.

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -8,11 +8,22 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            ChessPosition position = new ChessPosition('h', 1);
-            Console.WriteLine(position);
+            try
+            {
+                CBoard board = new CBoard(8, 8);
 
-            Console.WriteLine(position.ToPosition());
+                board.PlayPiece(new Rook(board, Color.Black), new Position(0, 0));
+                board.PlayPiece(new Rook(board, Color.Black), new Position(1, 3));
+                board.PlayPiece(new King(board, Color.Black), new Position(0, 7));
 
+                board.PlayPiece(new Rook(board, Color.White), new Position(3, 4));
+
+                View.PrintBoard(board);
+            }
+            catch(BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
--- Chess/View.cs
+++ Chess/View.cs
@@ -8,6 +8,8 @@ namespace Chess
         {
             for(int i = 0; i < board.Rows; i++)
             {
+                Console.Write(8 - i + " ");
+
                 for(int j = 0; j < board.Columns; j++)
                 {
                     if (board.piece(i, j) == null)
@@ -16,14 +18,28 @@ namespace Chess
                     }
                     else
                     {
-                        Console.Write(board.piece(i, j) + " ");
+                        PrintPiece(board.piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
         }
-        public View()
+
+        public static void PrintPiece(Piece piece)
         {
+            if (piece.Color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+            }
         }
     }
 }
